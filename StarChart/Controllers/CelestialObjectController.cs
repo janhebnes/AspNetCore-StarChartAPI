2392911eae11e9aed8ddd82a9d5e5fa2a38dd46c
Jan Hebnes <jan.hebnes@gmail.com>
2021-12-03 23:16:47 +0100

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
 namespace StarChart.Controllers
 {
-    [Route("")][ApiController]
+    [Route("")]
+    [ApiController]
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context) => _context = context;
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var model = _context.CelestialObjects.Find(id);
+            if (model == null)
+                return NotFound();
+
+            model.Satellites = _context.CelestialObjects.Where(d => d.OrbitedObjectId == model.Id).ToList();
+            return Ok(model);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var models = _context.CelestialObjects.Where(c => c.Name == name).ToList();
+            if (models.Count == 0)
+                return NotFound();
+
+            foreach (var model in models)
+            {
+                model.Satellites = _context.CelestialObjects.Where(d => d.OrbitedObjectId == model.Id).ToList();
+            }
+
+            return Ok(models);
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            var models = _context.CelestialObjects.ToList();
+            foreach (var model in models)
+            {
+                model.Satellites = _context.CelestialObjects.Where(d => d.OrbitedObjectId == model.Id).ToList();
+            }
+            return Ok(models);
+        }
     }
 }
